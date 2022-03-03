5d64a1b929a296f8dea239a34aeda827c940ca0a
vamsikrishna5737 <98752676+vamsikrishna5737@users.noreply.github.com>
2022-03-03 11:38:36 +0530

    candrivepassed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,9 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (hasDrivingLiscence==false) return "You cannot drive"
+    else if(isTired) return "You shouldn't drive"
+    else if(isSober) return "You can drive"
+    else return  "You shouldn't drive"
 }
 
 module.exports = CanDrive;
