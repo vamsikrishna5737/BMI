42ce626ff1372437f6dae64c7e51d1c563520484
vamsikrishna5737 <98752676+vamsikrishna5737@users.noreply.github.com>
2022-03-21 23:14:21 +0530

    Update script.js

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -57,20 +57,4 @@ document.querySelector('#pend').onclick=function(){
 
 }
 
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
-const list = document.querySelector(".todos");
-let count = 0;
-btn.addEventListener("click", function () {
-  // console.log(input.value);
-  // list.innerHTML = `<p class="${count}">${input.value}</p>`;
-  const para = document.createElement("p");
-  para.innerHTML = `${input.value}`;
-  list.appendChild(para);
-  para.setAttribute("key", `${count}`);
-  count++;
-});
-list.addEventListener("click", function (e) {
-  e.target.remove();
-});
 
